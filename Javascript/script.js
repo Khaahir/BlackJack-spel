da9fb51df571db7b559a6750922fc1b6a21b00e5
ameliesalo <145265503+ameliesalo@users.noreply.github.com>
2024-10-21 13:36:45 +0200

    first

---
+++ Javascript/script.js
@@ -0,0 +1,19 @@
+//Hämta korten 
+
+
+//Slumpa fram ett kort som visas i #card html (#cards är display: none)
+
+
+//Hämta knappen #again
+
+
+//Med slump functionen få den att generera ett kort vid click
+
+
+//En counter som registrerar kortets värde och + på nästa värde
+
+
+//Hämta knappen #stop
+
+
+//function jämför vem som är närmast 21
