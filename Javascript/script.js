a5e498126cfb14621eccb3b31ef27a010a1f8d25
ameliesalo <145265503+ameliesalo@users.noreply.github.com>
2024-10-22 10:10:13 +0200

    value and dealing fnctions

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,4 +1,9 @@
 //kortleken
+window.onload = function(){
+    builddeck();
+    shuffleDeck();
+    startGame()
+    }
 // nu ska den bara blandas...
 function builddeck() {
   let value = [
@@ -27,18 +32,59 @@ function builddeck() {
   console.log(deck);
 }
 
-builddeck();
 
 //Slumpa fram ett kort som visas i #card html (#cards är display: none)
-for(let i = 0; i <= cards.length; i++){
-    const randomCard = Math.floor(Math.random(cards[i]))
+function shuffleDeck(){
 }
 //Hämta knappen #again
 
 //Med slump functionen få den att generera ett kort vid click
+function startGame (){
+    hidden = deck.pop;
+    dealerSum += getValue(hidden);
+
+    dealerAceCount += checkAce(hidden);
+
+    while (dealerSum < 17){
+        let cardImg = document.createElement("img");
+        let card = deck.pop();
+        cardImg.src = "./Cards/" + card + "png";
+        dealerSum += getValue(card);
+        dealerAceCount += checkAce(card);
+        document.getElementById("dealer-cards").append(cardImg);
+    }
+
+    for (let i = 0; i < 2; i++){
+        let cardImg = document.createElement("img");
+        let card = deck.pop();
+        cardImg.src = "./Cards/" + card + "png";
+        yourSum += getValue(card);
+        yourAceCount += checkAce(card);
+        document.getElementById("your-cards").append(cardImg);
+    }
+}
 
 //En counter som registrerar kortets värde och + på nästa värde
+function getValue(card){
+    let data = card.split("-");
+    let value = data[0];
 
+        if (isNaN(value)){
+            if (value == "A"){
+                return 11;
+            }
+            return 10;
+        }
+
+        return parseInt(value);
+}
+
+function checkAce(card){
+    if (card[0] == "A"){
+        return 1;
+    }
+    return 0;
+}
 //Hämta knappen #stop
 
 //function jämför vem som är närmast 21
